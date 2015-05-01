08bfc61a29957583adc3db8d7a74baf0cdb2e211
lstephen <levi.stephen@gmail.com>
2015-05-01 12:07:31 -0700

    Add restore option to entrypoint

--- latest/jenkins-entrypoint.sh
+++ latest/jenkins-entrypoint.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 set -e
 
+if [[ -n "$JENKINS_RESTORE_FROM" ]]
+then
+  echo "Restoring from ${JENKINS_RESTORE_FROM}..."
+  curl $JENKINS_RESTORE_FROM | tar xvf -C $JENKINS_HOME
+fi
+
 echo "Copying plugins..."
 mkdir -p ${JENKINS_HOME}/plugins
+rm -rf ${JENKINS_HOME}/plugins/*
 cp ${JENKINS_PLUGINS_HOME}/* ${JENKINS_HOME}/plugins || :
 
 exec "$@"
