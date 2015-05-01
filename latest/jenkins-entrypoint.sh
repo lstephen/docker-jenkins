648ff35a5dc571cdadb119ffe3146c3a89f3cc5e
lstephen <levi.stephen@gmail.com>
2015-04-30 22:35:52 -0700

    Update jenkins entrypoint

--- latest/jenkins-entrypoint.sh
+++ latest/jenkins-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "Copying plugins..."
 mkdir -p ${JENKINS_HOME}/plugins
-rm ${JENKINS_HOME}/*
+rm ${JENKINS_HOME}/plugins/*
 cp ${JENKINS_PLUGINS_HOME}/* ${JENKINS_HOME}/plugins || :
 
 exec "$@"
